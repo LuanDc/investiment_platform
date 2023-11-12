98fc018612348157d2cc04fbdf396c1d95b57df9
Luan Fellipe <luan.fellipe@live.com>
2023-11-12 14:46:35 -0300

    test: should exclude quotes below the given date

--- test/investiment_platform/stocks_test.exs
+++ test/investiment_platform/stocks_test.exs
@@ -17,5 +17,20 @@ defmodule InvestimentPlatform.StocksTest do
 
       assert result == 40.0
     end
+
+    test "should exclude quotes below the given date" do
+      attrs = %{ticker: "TICKER01", date: "2023-11-07", price: 40.0}
+      stock_quote = insert(:stock_quote, attrs)
+
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", price: 30.0}
+      stock_quote = insert(:stock_quote, attrs)
+
+      ticker = "TICKER01"
+      start_date = "2023-11-08"
+
+      result = Stocks.get_max_quote(ticker, start_date)
+
+      assert result == 30.0
+    end
   end
 end
