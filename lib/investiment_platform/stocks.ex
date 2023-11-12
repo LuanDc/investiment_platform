1c80c10e445e138e7b18e172e878bdc227ec58a5
Luan Fellipe <luan.fellipe@live.com>
2023-11-12 15:09:29 -0300

    doc: create documentation for InvestimentPlatform.Stocks module

--- lib/investiment_platform/stocks.ex
+++ lib/investiment_platform/stocks.ex
@@ -1,12 +1,25 @@
 defmodule InvestimentPlatform.Stocks do
-  @moduledoc false
+  @moduledoc """
+  This module is responsible for exposing functions about stocks exchange context.
+  """
 
   import Ecto.Query
 
   alias InvestimentPlatform.Repo
   alias InvestimentPlatform.Stocks.StockQuote
 
-  @doc false
+  @doc """
+  Returns the maximum daily stock quote. If no stock quotes is found, returns nil.
+
+  ## Example
+
+    iex> Stocks.get_max_quote("TICKER1", "2023-11-10")
+    40.0
+
+    iex> Stocks.get_max_quote("TICKER2", "2023-11-10")
+    nil
+
+  """
   @spec get_max_quote(String.t(), String.t()) :: integer() | nil
   def get_max_quote(ticker, start_date) do
     query =
@@ -17,7 +30,18 @@ defmodule InvestimentPlatform.Stocks do
     Repo.one(query)
   end
 
-  @doc false
+  @doc """
+  Returns the maximum daily stock quote. If no stock quotes is found, returns nil.
+
+  ## Example
+
+    iex> Stocks.get_max_daily_volume("TICKER1", "2023-11-10")
+    20
+
+    iex> Stocks.get_max_daily_volume("TICKER2", "2023-11-10")
+    nil
+
+  """
   @spec get_max_daily_volume(String.t(), String.t()) :: integer() | nil
   def get_max_daily_volume(ticker, date) do
     sub_query =
