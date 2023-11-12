83ee24dcf24040632181e0e590644737ec5d3934
Luan Fellipe <luan.fellipe@live.com>
2023-11-12 14:57:27 -0300

    test: should return the maximum daily volume from the date provided until current day

--- test/investiment_platform/stocks_test.exs
+++ test/investiment_platform/stocks_test.exs
@@ -8,7 +8,7 @@ defmodule InvestimentPlatform.StocksTest do
   describe "get_max_quote/2" do
     test "should return the maximum quota from the date provided until current day" do
       attrs = %{ticker: "TICKER01", date: "2023-11-08", price: 40.0}
-      stock_quote = insert(:stock_quote, attrs)
+      insert(:stock_quote, attrs)
 
       ticker = "TICKER01"
       start_date = "2023-11-08"
@@ -20,10 +20,10 @@ defmodule InvestimentPlatform.StocksTest do
 
     test "should exclude quotes below the given date" do
       attrs = %{ticker: "TICKER01", date: "2023-11-07", price: 40.0}
-      stock_quote = insert(:stock_quote, attrs)
+      insert(:stock_quote, attrs)
 
       attrs = %{ticker: "TICKER01", date: "2023-11-08", price: 30.0}
-      stock_quote = insert(:stock_quote, attrs)
+      insert(:stock_quote, attrs)
 
       ticker = "TICKER01"
       start_date = "2023-11-08"
@@ -35,10 +35,10 @@ defmodule InvestimentPlatform.StocksTest do
 
     test "should exclude ticker other than the one provided" do
       attrs = %{ticker: "TICKER02", date: "2023-11-08", price: 40.0}
-      stock_quote = insert(:stock_quote, attrs)
+      insert(:stock_quote, attrs)
 
       attrs = %{ticker: "TICKER01", date: "2023-11-08", price: 30.0}
-      stock_quote = insert(:stock_quote, attrs)
+      insert(:stock_quote, attrs)
 
       ticker = "TICKER01"
       start_date = "2023-11-08"
@@ -53,6 +53,70 @@ defmodule InvestimentPlatform.StocksTest do
       start_date = "2023-11-08"
 
       result = Stocks.get_max_quote(ticker, start_date)
+
+      assert result == nil
+    end
+  end
+
+  describe "get_max_daily_volume/2" do
+    test "should return the maximum daily volume from the date provided until current day" do
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", amount: 80}
+      insert(:stock_quote, attrs)
+
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", amount: 20}
+      insert(:stock_quote, attrs)
+
+      ticker = "TICKER01"
+      start_date = "2023-11-08"
+
+      result = Stocks.get_max_daily_volume(ticker, start_date)
+
+      assert result == 100
+    end
+
+    test "should exclude quotes below the given date" do
+      attrs = %{ticker: "TICKER01", date: "2023-11-07", amount: 80}
+      insert(:stock_quote, attrs)
+      attrs = %{ticker: "TICKER01", date: "2023-11-07", amount: 20}
+      insert(:stock_quote, attrs)
+
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", amount: 50}
+      insert(:stock_quote, attrs)
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", amount: 30}
+      insert(:stock_quote, attrs)
+
+      ticker = "TICKER01"
+      start_date = "2023-11-08"
+
+      result = Stocks.get_max_daily_volume(ticker, start_date)
+
+      assert result == 80
+    end
+
+    test "should exclude ticker other than the one provided" do
+      attrs = %{ticker: "TICKER02", date: "2023-11-08", amount: 80}
+      insert(:stock_quote, attrs)
+      attrs = %{ticker: "TICKER02", date: "2023-11-08", amount: 20}
+      insert(:stock_quote, attrs)
+
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", amount: 50}
+      insert(:stock_quote, attrs)
+      attrs = %{ticker: "TICKER01", date: "2023-11-08", amount: 30}
+      insert(:stock_quote, attrs)
+
+      ticker = "TICKER01"
+      start_date = "2023-11-08"
+
+      result = Stocks.get_max_daily_volume(ticker, start_date)
+
+      assert result == 80
+    end
+
+    test "should return null when no stock quote is found" do
+      ticker = "TICKER01"
+      start_date = "2023-11-08"
+
+      result = Stocks.get_max_daily_volume(ticker, start_date)
 
       assert result == nil
     end
