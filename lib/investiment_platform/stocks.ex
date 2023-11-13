1d6de70375ece1be9307519024e894ba018a23c3
Luan Fellipe <luan.fellipe@live.com>
2023-11-13 01:17:16 -0300

    refact: creates guardian clause

--- lib/investiment_platform/stocks.ex
+++ lib/investiment_platform/stocks.ex
@@ -21,7 +21,7 @@ defmodule InvestimentPlatform.Stocks do
 
   """
   @spec get_max_quote(String.t(), String.t()) :: integer() | nil
-  def get_max_quote(ticker, start_date) do
+  def get_max_quote(ticker, start_date) when is_binary(ticker) and is_binary(start_date) do
     query =
       from sq in StockQuote,
         where: sq.ticker == ^ticker and sq.date >= ^start_date,
@@ -43,10 +43,10 @@ defmodule InvestimentPlatform.Stocks do
 
   """
   @spec get_max_daily_volume(String.t(), String.t()) :: integer() | nil
-  def get_max_daily_volume(ticker, date) do
+  def get_max_daily_volume(ticker, start_date) when is_binary(ticker) and is_binary(start_date) do
     sub_query =
       from stq in StockQuote,
-        where: stq.ticker == ^ticker and stq.date >= ^date,
+        where: stq.ticker == ^ticker and stq.date >= ^start_date,
         group_by: stq.date,
         select: %{amount: sum(stq.amount)}
 
